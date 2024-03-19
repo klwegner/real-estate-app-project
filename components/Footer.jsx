666850ab57af2f1194b7efc9d32dc095ebaea105
Kristen Wegner <klwegner@gmail.com>
2024-03-19 11:40:42 -0400

    skeleton of search page created

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/react';
+
+const Footer =() => (
+    <Box textAlign="center" p="5" color="gray.600" borderTop="1" borderColor="gray.100">
+{new Date().getFullYear()} Kristen Real Estate Enterprises, Inc.
+    </Box>
+)
+
+export default Footer;
