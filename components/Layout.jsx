bd3b05ebe1024a6cb9aa56e6d22a063860b2a9ef
Kristen Wegner <klwegner@gmail.com>
2024-03-19 10:50:15 -0400

    Project approx. 1/4 finished; aligns with min. 48 of video. Properties rendering. Styling needed.

---
+++ components/Layout.jsx
@@ -0,0 +1,8 @@
+//a start
+
+const Layout = () => {
+   
+}
+
+
+export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
 import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import Router from 'next/router';
+import Head from 'next/head';
+import NProgress from 'nprogress';
+import Layout from '../components/Layout.jsx';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
