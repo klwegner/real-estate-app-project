bbf5be3f1d5d7984d58e2f05335f661b20c5a3c8
Kristen Wegner <klwegner@gmail.com>
2024-04-24 11:38:54 -0400

    more apostrophes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -69,7 +69,7 @@ const Navbar = () => {
               <Spacer />
               <>
                 <Text as="i" fontSize="xl" color="blue.400" fontWeight="bold">
-                  Your Key to Tampa Bay's Hottest Homes, Apartments, Townhomes,
+                  Your Key to Tampa Bay&apos;s Hottest Homes, Apartments, Townhomes,
                   and Condos
                 </Text>
               </>
